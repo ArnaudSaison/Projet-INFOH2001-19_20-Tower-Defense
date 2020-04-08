54bed776779e89c230683d1821634f02e58fb18e
ArnaudS <arnaud.saison@ulb.be>
2020-04-08 11:57:21 +0200

    menus fonctionnels de la gui avec JXML

---
+++ src/towerdefense/gui/map/MapGUI.java
@@ -0,0 +1,16 @@
+package towerdefense.gui.map;
+
+import javafx.scene.layout.StackPane;
+
+public class MapGUI extends StackPane {
+
+    public void draw(){
+
+    }
+
+    public void intialize(){
+
+    }
+
+
+}
--- src/towerdefense/gui/map/editor/MapEditorController.java
+++ src/towerdefense/gui/map/editor/MapEditorController.java
@@ -3,6 +3,7 @@ package towerdefense.gui.map.editor;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import towerdefense.MainApplication;
 import towerdefense.gui.generic.GUIController;
 
@@ -12,10 +13,13 @@ import java.util.ResourceBundle;
 
 public class MapEditorController implements Initializable, GUIController {
     private MainApplication mainApplication;
+    private boolean isSaved;
+
+    @FXML private StackPane mapPlaceHolder;
 
     // Initialisation du controller
     public MapEditorController(){
-
+        isSaved = false;
     }
 
     @Override
@@ -30,9 +34,23 @@ public class MapEditorController implements Initializable, GUIController {
     // Gestion des éléments FXML
     @FXML
     public void handleBackToMenuButtonClicked(MouseEvent event) throws IOException {
-        boolean answer = mainApplication.confirmWindow("Unsaved changes will be lost. Are you sure you want to exit the editor ?", "Yes", "No", "Confirm");
-        if (answer){
+        if (!isSaved) {
+            boolean answer = mainApplication.confirmWindow("Unsaved changes will be lost. Are you sure you want to exit the editor ?", "Yes", "No", "Confirm");
+            if (answer){
+                mainApplication.setCurrentSceneTo(MainApplication.SceneType.MENU);
+            }
+        } else {
             mainApplication.setCurrentSceneTo(MainApplication.SceneType.MENU);
         }
     }
+
+    @FXML
+    public void handleSaveButtonClicked(MouseEvent event) throws IOException {
+        isSaved = true;
+    }
+
+
+
+    // Logique de controller
+
 }
