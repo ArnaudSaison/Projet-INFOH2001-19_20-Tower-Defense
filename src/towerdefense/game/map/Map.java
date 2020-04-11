8f3c1420433683beaa4368ef49ae69674ad9ba9d
ArnaudS <arnaud.saison@ulb.be>
2020-04-11 15:06:42 +0200

    package map

---
+++ src/towerdefense/game/map/Map.java
@@ -0,0 +1,4 @@
+package towerdefense.game.map;
+
+public class Map {
+}
--- src/towerdefense/gui/map/MapGUI.java
+++ src/towerdefense/gui/map/MapGUI.java
@@ -11,19 +11,19 @@ public class MapGUI extends StackPane {
 
     public void MapGUI(boolean dynamic){
 
-        if (dynamic){
-            Timeline timer = new Timeline(new KeyFrame(Duration.millis(500), new EventHandler<ActionEvent>() {
-
-                @Override
-                public void handle(ActionEvent event) {
-                    System.out.println("Check");
-                    for(Shape c: shapes){
-                        c.update();
-                    }
-
-                }
-            }));
-        }
+//        if (dynamic){
+//            Timeline timer = new Timeline(new KeyFrame(Duration.millis(500), new EventHandler<ActionEvent>() {
+//
+//                @Override
+//                public void handle(ActionEvent event) {
+//                    System.out.println("Check");
+//                    for(Shape c: shapes){
+//                        c.update();
+//                    }
+//
+//                }
+//            }));
+//        }
     }
 
     public void intialize(){
