348cf89011ed666eac2bf7ba61cbd5c7426f000f
ArnaudS <arnaud.saison@ulb.be>
2020-04-11 15:16:24 +0200

    package map

--- src/towerdefense/game/map/Map.java
+++ src/towerdefense/game/map/Map.java
@@ -1,4 +1,11 @@
 package towerdefense.game.map;
 
+import java.util.ArrayList;
+
 public class Map {
+    private ArrayList<> tiles;
+
+    public Map(){
+
+    }
 }
--- src/towerdefense/game/map/Tile.java
+++ src/towerdefense/game/map/Tile.java
@@ -1,4 +1,31 @@
 package towerdefense.game.map;
 
 public class Tile {
+    private int tileX;
+    private int tileY;
+
+    private
+
+    public Tile (){
+        tileX = 0;
+        tileY = 0;
+    }
+
+
+
+    public int getTileX(){
+        return tileX;
+    }
+
+    public int getTileY(){
+        return tileY;
+    }
+
+    public void setTileX(int tileX){
+        this.tileX = tileX;
+    }
+
+    public void setTileY(int tileY){
+        this.tileY = tileY;
+    }
 }
